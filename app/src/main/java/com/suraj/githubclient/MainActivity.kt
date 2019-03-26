c826c7aa54c5fb903cd1208dfa31348e845c59a3
suraj <suraj2600@gmail.com>
2019-03-26 22:11:18 +0530

    adding dimen values in xml under res folder

--- app/src/main/java/com/suraj/githubclient/MainActivity.kt
+++ app/src/main/java/com/suraj/githubclient/MainActivity.kt
@@ -3,6 +3,9 @@ package com.suraj.githubclient
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
+
+/*Created by suraj on 26/03/2019*/
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
